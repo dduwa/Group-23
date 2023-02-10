3b4cd17872ad9e7e8705b1986de1c8524f6caf90
farandead <92890148+farandead@users.noreply.github.com>
2023-02-10 14:34:07 +0000

    Add files via upload

---
+++ KingsMen/src/main/java/com/example/KingsMen/KingsMenApplication.java
@@ -0,0 +1,13 @@
+package com.example.KingsMen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingsMenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingsMenApplication.class, args);
+	}
+
+}
---
+++ KingsMen/src/test/java/com/example/KingsMen/KingsMenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KingsMen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingsMenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
